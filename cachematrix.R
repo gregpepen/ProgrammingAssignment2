e813c53e5c95a4355a856e3659c2759d40f38e0d
MERILIO GREGORIO MORELL <morell.merilio@gmail.com>
2020-10-08 17:11:13 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions is used to demostrated the use and importance 
+# #of LEXICAL SCOPE in R. The function create special "matrix" object
+#and cache its inverse.Submitted by merilio morell
+
+## The function create special "matrix" object and cache its inverse.
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
+ ## Return a matrix that is the inverse of 'x'
+    ## This function uses the  functions cache in the "makeCacheMatrix" above and 
+##find use the stored data if the imput is the same already used, if not it evaluate
+#the imput (matrix) and return its inverse 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return (m)
+        
 }
+
